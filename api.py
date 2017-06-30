feef616158bc1654486bc95e65185feee0458f48
Juzley <juzley@gmail.com>
2017-06-30 17:58:29 +0100

    Faffing with the API

--- api.py
+++ api.py
@@ -1,11 +1,13 @@
-from flask_security import login_required, current_user, roles_required
-from flask import render_template, request, redirect, url_for, jsonify
+from flask_security import login_required, current_user
+from flask_api import status
+from flask import request, jsonify
 import json
 
 from questions import questionsmanager
 from model import Event, EventAdmin, Team, Answer, Participant
 from model import get_team, is_event_admin
 from app import app
+
 
 class InvalidUsage(Exception):
     status_code = 400
@@ -22,12 +24,14 @@ class InvalidUsage(Exception):
         rv['message'] = self.message
         return rv
 
+
 class EventNotFound(InvalidUsage):
     def __init__(self, event_id):
         InvalidUsage.__init__(
             self,
             message='Event with id {} does not exist'.format(event_id),
             status_code=404)
+
 
 class TeamNotFound(InvalidUsage):
     def __init__(self, name, event_id):
@@ -37,11 +41,13 @@ class TeamNotFound(InvalidUsage):
                                                                     event_id),
             status_code=404)
 
+
 @app.errorhandler(InvalidUsage)
 def handle_invalid_usage(error):
     response = jsonify(error.to_dict())
     response.status_code = error.status_code
     return response
+
 
 @app.route('/create_event/<questions>/<name>')
 @login_required
@@ -101,6 +107,7 @@ def end_event(event_id):
 
     return ('', 200)
 
+
 @app.route('/get_event_info/<event_id>')
 def get_event_info(event_id):
     event = Event.get(Event.id == event_id)
@@ -118,9 +125,9 @@ def get_event_info(event_id):
         teams.append({'name': t.name,
                       'members': members})
 
-    data = { 'name': event.name,
-             'id': event.id,
-             'teams': teams }
+    data = {'name': event.name,
+            'id': event.id,
+            'teams': teams}
 
     return json.dumps(data)
 
@@ -200,37 +207,6 @@ def leave_team(event_id, name):
     return ('', 200)
 
 
-@app.route('/answer_question/<event_id>/<question>', methods=['POST'])
-@login_required
-def answer_question(event_id, question):
-    answer = request.get_json()
-
-    event = Event.get(Event.id == event_id)
-    if not event:
-        raise InvalidUsage(
-            'Event with id {} does not exist'.format(event_id), 404)
-
-    if not event.active:
-        raise InvalidUsage(
-            'Event id {} has not started'.format(event_id), 403)
-
-    team = get_team(event, current_user)
-    if not team:
-        raise InvalidUsage(
-            'User {} is not on a team'.format(current_user.id), 403)
-
-    questions = questionsmanager.getPluginByName(event.questions)
-    # TODO: Questions not found
-
-    a, _ = Answer.get_or_create(team=team, question=question)
-    a.answer = answer
-    a.correct = questions.plugin_object.check_answer(question, answer)
-    a.save()
-
-    # No data to return
-    return ('', 204)
-
-
 @app.route('/get_answers/<event_id>')
 @login_required
 def get_answers(event_id):
@@ -248,3 +224,62 @@ def get_answers(event_id):
                Answer.select().where(Answer.team == team.id)}
     return json.dumps(answers)
 
+
+@app.route('/api/v1/events/<event_id>/answers/', methods=['GET'])
+@login_required
+def answers_get(event):
+    event = Event.get(Event.id == event_id)
+    if not event:
+        return ('Event with id {} does not exist'.format(event_id),
+                status.HTTP_404_NOT_FOUND)
+
+    # TODO: Support filtering for specific questoins
+    # TODO: Need the option to get answers for all teams after the event has
+    # finished.
+
+    team = get_team(event, current_user)
+    if not team:
+        return ('User {} is not on a team'.format(current_user.id),
+                status.HTTP_400_BAD_REQUEST)
+
+    response = {'answers': {a.question: a.answer for a in
+                   Answer.select().where(Answer.team == team.id)}}
+
+
+@app.route('/api/v1/events/<event_id>/answers/',
+           methods=['POST', 'PUT', 'PATCH'])
+@login_required
+def answers_update(event_id):
+    event = Event.get(Event.id == event_id)
+    if not event:
+        return ('Event with id {} does not exist'.format(event_id),
+                status.HTTP_404_NOT_FOUND)
+
+    if not event.active:
+        return ('Event with id {} has not started'.format(event_id),
+                status.HTTP_400_BAD_REQUEST)
+
+    team = get_team(event, current_user)
+    if not team:
+        return ('User {} is not on a team'.format(current_user.id),
+                status.HTTP_400_BAD_REQUEST)
+
+    data = request.get_json()
+    if 'answers' not in data:
+        return ('Data must contain "answers" object',
+                status.HTTP_400_BAD_REQUEST)
+
+    for question, answer in data['answers']:
+        # TODO: Need to check that the question exists. 
+        a, _ = Answer.get_or_create(team=team, question=question)
+        a.answer = answer
+
+        # TODO: Don't store correctness in the DB, check it when we need to know
+        a.correct = questions.plugin_object.check_answer(question, answer)
+        a.save()
+
+
+    # TODO: Just return the updated answers.
+    response = {'answers': {a.question: a.answer for a in
+                   Answer.select().where(Answer.team == team.id)}}
+    return json.dumps(answers)
